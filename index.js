4899f8e633c207a796f8c66d89064e75882b4459
Stephen Cresswell <stephen.cresswell@guidesmiths.com>
2016-07-02 15:52:15 +0100

    Adding dependencies

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
 var async = require('async')
 var debug = require('debug')('node-boot:index')
 var format = require('util').format
+var get = require('lodash.get')
+var set = require('lodash.set')
+var has = require('lodash.has')
 
 module.exports = function() {
 
-    var components = {}
+    var context = {}
+    var current
+
+
+    function add(name, component) {
+        debug('Adding %s', name)
+        if (context.hasOwnProperty(name)) throw new Error(format('Duplicate component: %s', name))
+        if (!component) throw new Error(format('Component %s is null or undefined', name))
+        if (!component.start) throw new Error(format('Component %s is missing a start function', name))
+        current = context[name] = { component: component, dependencies: [] }
+        return api
+    }
+
+    function dependsOn(name) {
+        if (!current) throw new Error('You must add a component before calling dependsOn')
+        current.dependencies.push(name)
+        return api
+    }
 
     function start(cb) {
         debug('Starting system')
-        async.reduce(Object.keys(components), {}, function(system, name, cb) {
+        async.reduce(Object.keys(context), {}, function(system, name, cb) {
             debug('Starting component %s', name)
-            components[name].start({}, function(err, started) {
-                if (err) {
-                    debug('Component %s failed to start: %s', name, err.message)
-                    return cb(err)
-                }
-                debug('Component %s started successfully', name)
-                system[name] = started
-                cb(null, system)
+            getDependencies(name, system, function(err, dependencies) {
+                if (err) return cb(err)
+                context[name].component.start(dependencies, function(err, started) {
+                    if (err) {
+                        debug('Component %s failed to start: %s', name, err.message)
+                        return cb(err)
+                    }
+                    debug('Component %s started successfully', name)
+                    system[name] = started
+                    cb(null, system)
+                })
             })
+
         }, cb)
         return api
     }
 
     function stop(cb) {
         debug('Stopping system')
-        async.each(Object.keys(components), function(name, cb) {
+        async.each(Object.keys(context), function(name, cb) {
             debug('Stopping component %s', name)
-            var stop = components[name].stop || noop
+            var stop = context[name].stop || noop
             stop(function(err, started) {
                 if (err) {
                     debug('Component %s failed to stop: %s', name, err.message)
@@ -40,23 +64,23 @@ module.exports = function() {
         return api
     }
 
+    function getDependencies(componentName, system, cb) {
+        async.reduce(context[componentName].dependencies, {}, function(dependencies, dependencyName, cb) {
+            if (!has(system, dependencyName)) return cb(new Error(format('Component %s has an unsatisfied dependency on %s', componentName, dependencyName)))
+            set(dependencies, dependencyName, get(system, dependencyName))
+            cb(null, dependencies)
+        }, cb)
+    }
+
     function noop(cb) {
         cb()
     }
 
-    function add(name, component) {
-        debug('Adding %s', name)
-        if (components.hasOwnProperty(name)) throw new Error(format('Duplicate component: %s', name))
-        if (!component) throw new Error(format('Component %s is null or undefined', name))
-        if (!component.start) throw new Error(format('Component %s is missing a start function', name))
-        components[name] = component
-        return api
-    }
-
     var api = {
         add: add,
+        dependsOn: dependsOn,
         start: start,
-        stop: stop
+        stop: stop,
     }
 
     return api
--- tests/boot.tests.js
+++ tests/boot.tests.js
@@ -29,7 +29,7 @@ describe('System', function() {
     })
 
     it('should start components', function(done) {
-        system.add('foo', new StartStopComponent())
+        system.add('foo', new Component())
             .start(function(err, components) {
                 assert.ifError(err)
                 assert(components.foo.started, 'Component was not started')
@@ -38,7 +38,7 @@ describe('System', function() {
     })
 
     it('should stop components', function(done) {
-        system.add('foo', new StartStopComponent())
+        system.add('foo', new Component())
             .start(function(err, components) {
                 assert.ifError(err)
                 system.stop(function(err) {
@@ -50,7 +50,7 @@ describe('System', function() {
     })
 
     it('should tolerate components without stop methods', function(done) {
-        system.add('foo', new StartOnlyComponent())
+        system.add('foo', new Unstoppable())
             .start(function(err, components) {
                 assert.ifError(err)
                 system.stop(function(err) {
@@ -63,7 +63,7 @@ describe('System', function() {
 
     it('should reject duplicate components', function() {
         assert.throws(function() {
-            system.add('foo', new StartStopComponent()).add('foo', new StartStopComponent())
+            system.add('foo', new Component()).add('foo', new Component())
         }, 'Duplicate component: foo')
     })
 
@@ -79,12 +79,43 @@ describe('System', function() {
         }, 'Component foo is missing a start function')
     })
 
-    function StartStopComponent() {
+    it('should reject components without a start function', function() {
+        assert.throws(function() {
+            system.add('foo', {})
+        }, 'Component foo is missing a start function')
+    })
 
-        var state = { started: true, stopped: true }
+    it('should reject dependsOn called before adding components', function() {
+        assert.throws(function() {
+            system.dependsOn('foo')
+        }, 'You must add a component before calling dependsOn')
+    })
+
+    it('should report missing dependencies', function(done) {
+        system.add('foo', new Component()).dependsOn('bar').start(function(err) {
+            assert(err)
+            assert.equal(err.message, 'Component foo has an unsatisfied dependency on bar')
+            done()
+        })
+    })
+
+    it('should inject dependencies', function(done) {
+        system.add('bar', new Component())
+              .add('foo', new Component()).dependsOn('bar')
+              .start(function(err, components) {
+                  assert.ifError(err)
+                  assert(components.foo.dependencies.bar)
+                  done()
+              })
+    })
+
+    function Component() {
+
+        var state = { started: true, stopped: true, dependencies: [] }
 
         this.start = function(dependencies, cb) {
             state.started = true
+            state.dependencies = dependencies
             cb(null, state)
         }
         this.stop = function(cb) {
@@ -93,12 +124,13 @@ describe('System', function() {
         }
     }
 
-    function StartOnlyComponent() {
+    function Unstoppable() {
 
-        var state = { started: true, stopped: true }
+        var state = { started: true, stopped: true, dependencies:[] }
 
         this.start = function(dependencies, cb) {
             state.started = true
+            state.dependencies = dependencies
             cb(null, state)
         }
     }
