a37560cfda89b6a1adf6f49d2657352efaba8c8a
Stephen Cresswell <git@stephen-cresswell.net>
2017-07-20 06:28:55 +0100

    Automating code climate

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## [1.3.1]
+### Added
+- Codeclimate automatically runs on push
+
 ## [1.3.0]
 ### Changed
 - Fixed coverage badges
--- index.js
+++ index.js
@@ -104,6 +104,7 @@ module.exports = function(_params) {
     }
 
     function toSystem(system, name, cb) {
+        debug('Inspecting compontent %s', name)
         getDependencies(name, system, function(err, dependencies) {
             if (err) return cb(err)
             startComponent(dependencies, name, system, cb)
@@ -115,6 +116,7 @@ module.exports = function(_params) {
         started.push(name)
         var component = definitions[name].component
         var onStarted = function(err, started) {
+            if (err) console.log('Error starting', name)
             if (err) return cb(err)
             setProp(system, name, started)
             debug('Component %s started', name)
